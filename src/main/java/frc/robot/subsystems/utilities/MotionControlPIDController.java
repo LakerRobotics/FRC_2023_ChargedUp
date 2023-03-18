7b684b53b67cf21d80214ebaeabb9cc8a2a40d04
Richard G. Topolewski <topolewr@cooley.edu>
2023-03-17 20:19:06 -0400

    Revert "New Balance based on speed set to Tilt angle"
    This reverts commit 0e2c508cc7b085b6259dc8ce85fe5a22a39aeb52.

--- src/main/java/frc/robot/subsystems/utilities/MotionControlPIDController.java
+++ src/main/java/frc/robot/subsystems/utilities/MotionControlPIDController.java
@@ -28,4 +28,27 @@ public class MotionControlPIDController extends PIDController {
 		motionControl.setRegularPIDControl(this);// to let the motionControl adjust the Rate, ie do the motion control
 	}
 	
+//OLD	public MotionControlPIDController(double Kp, double Ki, double Kd, AdjustSpeedAsTravelHelper motionControl, double period) 
+//OLD	{
+//OLD		//super(Kp, Ki, Kd, motionControl.getM_source(), motionControl.getM_output(), period);
+//OLD		super(Kp, Ki, Kd);
+//OLD		//TODO code needs to call calculate(motionControl.getM_source(), motionControl.getM_output(), period);
+//OLD		m_motionControlHelper = motionControl;
+//OLD		motionControl.setRegularPIDControl(this);// to let the motionControl adjust the Rate, ie do the motion control
+//OLD	}
+
+//OLD	public MotionControlPIDController(double Kp, double Ki, double Kd, double Kf, AdjustSpeedAsTravelHelper motionControl) 
+//OLD	{
+//OLD		//super(Kp, Ki, Kd, Kf, motionControl.getM_source(), motionControl.getM_output());
+//OLD		super(Kp, Ki, Kd, Kf, motionControl.getM_source(), motionControl.getM_output());
+//OLD		m_motionControlHelper = motionControl;
+//OLD		motionControl.setRegularPIDControl(this);// to let the motionControl adjust the Rate, ie do the motion control
+//OLD	}
+
+//OLD	public MotionControlPIDController(double Kp, double Ki, double Kd, double Kf, double period, AdjustSpeedAsTravelHelper motionControl) 
+//OLD	{
+//OLD		super(Kp, Ki, Kd, Kf, motionControl.getM_source(), motionControl.getM_output(), period);
+//OLD		m_motionControlHelper = motionControl; 
+//OLD		motionControl.setRegularPIDControl(this);// to let the motionControl adjust the Rate, ie do the motion control
+//OLD	}
 }
