d6a9393dc5ec8eca9e759e19624117dbc5425945
Richard G. Topolewski <topolewr@cooley.edu>
2023-03-19 16:02:05 -0400

    Reworked cod with help of ChatGPT to be faster by doing things in parrallel, still needs testing

--- src/main/java/frc/robot/commands/AutonomousPlaceDriveBalance.java
+++ src/main/java/frc/robot/commands/AutonomousPlaceDriveBalance.java
@@ -10,6 +10,7 @@
 
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ArmConstants;
@@ -29,28 +30,33 @@ public class AutonomousPlaceDriveBalance extends SequentialCommandGroup {
 
     public AutonomousPlaceDriveBalance(Intake theIntake, Arm theArm, DriveTrain theDriveTrain){
 
-// Pick up cone
 
-addCommands(new IntakeConeIn(theIntake).withTimeout(1));
+        // Pick up cone
+        addCommands(new IntakeConeIn(theIntake).withTimeout(0.25));
 
-// Extend arm 
+        // Extend arm and secure cone
+        ParallelCommandGroup extendArmWhileSecuringCone = new ParallelCommandGroup(
+            new ArmControlExtend(theArm).withTimeout(3),
+            new SequentialCommandGroup(
+                new IntakeConeIn(theIntake).withTimeout(0.75),
+                new IntakeConeHoldStart(theIntake).withTimeout(0.1)
+            )
+        );
+        addCommands(extendArmWhileSecuringCone);
 
-addCommands(new IntakeConeHoldStart(theIntake).withTimeout(1));
+        // Drop cone
+        addCommands(new IntakeConeOut(theIntake).withTimeout(2));
 
-addCommands(new ArmControlExtend(theArm).withTimeout(3));
+        // Retract arm
+        ParallelCommandGroup retractArmWhileDrivingBackwards = new ParallelCommandGroup(
+            new ArmControlRetract(theArm).withTimeout(3),
+            new DriveTrainBackwards(theDriveTrain).withTimeout(2)
+        );
+        addCommands(retractArmWhileDrivingBackwards);
 
-// Drop cone
-
-addCommands(new IntakeConeOut(theIntake).withTimeout(2));
-
-// Retract arm
-
-addCommands(new ArmControlRetract(theArm).withTimeout(3));
-
-// Drive backwards
-
-addCommands(new DriveTrainBackwards(theDriveTrain).withTimeout(2));
-
+        // Balance
+        addCommands(new DriveTrainBalance(theDriveTrain).withTimeout(6));
+    }
 // Drive Fowards
 
 //addCommands(new DriveTrainFowards(theDriveTrain).withTimeout(1.25));
